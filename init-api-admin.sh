bc5538faaf137dd6171485bd9d58299f0f5482c1
Oleg Kovalenko <monstrenyatko@gmail.com>
2017-10-04 23:01:40 -0500

    Fix scripts to init `butler-api` admin user.
    Pass password using environment variable to avoid logging.
    Make admin email configurable.

--- init-api-admin.sh
+++ init-api-admin.sh
@@ -14,14 +14,17 @@ if [ -n "$1" ]; then
 fi
 
 service='api'
-MANAGE="$COMPOSE -f docker-compose.yml run --rm -T $service manage.py"
-
-password=$(cat $BUTLER_HOME/api/django-admin-password)
+MANAGE="$COMPOSE -f docker-compose.yml run --rm -T \
+  -e BUTLER_API_DJANGO_ADMIN_EMAIL \
+  -e BUTLER_API_DJANGO_ADMIN_PASSWORD=$(cat $BUTLER_HOME/api/django-admin-password) \
+  $service manage.py"
 
 $MANAGE shell -c \
 "from django.contrib.auth.models import User;"\
-"User.objects.filter(email='admin@example.com').delete();"\
-"User.objects.create_superuser('admin', 'admin@example.com', '$password')"
+"User.objects.filter(username='admin').delete();"\
+"User.objects.create_superuser('admin',"\
+" os.environ['BUTLER_API_DJANGO_ADMIN_EMAIL'],"\
+" os.environ['BUTLER_API_DJANGO_ADMIN_PASSWORD'])"
 
 set +x
 echo $'\n'"[DONE] API admin"
